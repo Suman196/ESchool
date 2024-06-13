c6bf786a7f6036036775aa8eab6b743ad9f65738
Suman196 <sumansahasodepur@gmail.com>
2024-06-13 18:34:25 +0530

    Hosting

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,21 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+   header("Location: login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="logandsign.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>User Dashboard</title>
 </head>
 <body>
-    <nav class="navbar navbar-light bg-light ">
+<nav class="navbar navbar-light bg-light ">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
              
@@ -31,6 +38,9 @@
         </ul>    
         </div>
     </nav>
-    
+    <div class="container">
+        <h1>Welcome to Dashboard</h1>
+        <a href="logout.php" class="btn btn-warning">Logout</a>
+    </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: dashboard.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-light bg-light ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+             
+           ESchool
+          </a>
+        <ul class="nav justify-content-center">
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="course.html">Courses</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Log in</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="registration.php">Sign Up</a>
+              </li>
+        </ul>    
+        </div>
+    </nav>
+    <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: dashboard.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+        }
+        ?>
+
+      <form action="login.php" method="post">
+        <p id="login-welcome" style=" font-size: 30px;
+    display: flex;
+    justify-content: center;">Welcome back</p>
+        <div class="form-group">
+            <input type="email" placeholder="Enter Email" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Enter Password" name="password" class="form-control">
+        </div>
+        <div class="form-btn">
+            <input type="submit" value="Login" name="login" class="btn btn-primary">
+        </div>
+      </form>
+     <div><p>Not registered yet <a href="registration.php">Register Here</a></p></div>
+    </div>
+</body>
+</html>
